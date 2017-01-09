81b37fc4710b6dbe7a7f2587cd4c3d285ffe97de
Maria <maria-robobug@users.noreply.github.com>
2017-01-09 22:02:00 +0000

    Header data retrieved from json

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component {
 	render () {
 		return (
       <div className="App">
-        <Header />
+        <Header data={this.state.resumeData.main}/>
 				<About />
 				<Resume />
 				<Portfolio />
