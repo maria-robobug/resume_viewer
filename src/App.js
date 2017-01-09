edd01d7a1fd2edd97182e037663153268878abc9
Maria <maria-robobug@users.noreply.github.com>
2017-01-09 21:23:28 +0000

    Added data from json file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './App.css';
+import $ from 'jquery';
 
+import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -10,6 +11,32 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 class App extends React.Component {
+	constructor (props){
+		super(props);
+		this.state = {
+			foo: 'bar',
+			resumeData: {}
+		};
+	}
+	
+	getResumeData () {
+		$.ajax({
+			url: 'http://localhost:3000/resumeData.json',
+			dataType: 'json',
+			cache: false,
+			success: (data) => {
+				this.setState({resumeData: data});
+			},
+			error: (xhr, status, error) => {
+				alert(error);
+			}
+		});
+	}
+	
+	componentDidMount () {
+		this.getResumeData();
+	}
+	
 	render () {
 		return (
       <div className="App">
